f49fea5c2214a80b01ceea87252f785af0f1ecf4
JavierRamirez96 <157761489+JavierRamirez96@users.noreply.github.com>
2024-01-31 12:31:29 -0800

    commit Lab1/Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {This file prints the words "Hello World!" and "Hello World again!".}
+ ***/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
