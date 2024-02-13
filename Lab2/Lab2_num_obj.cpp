5df7fea376c42c57e6e0779516f0401f64266abf
JavierRamirez96 <157761489+JavierRamirez96@users.noreply.github.com>
2024-02-13 12:26:11 -0800

    commit Lab1/Hello.cpp

---
+++ Lab2/Lab2_num_obj.cpp
@@ -0,0 +1,14 @@
+//Lab2_num_obj.cpp
+/*
+
+*/
+
+#include <iostream>
+#include <ctsdlib>
+using namespace std;
+
+int main()
+    //numerical
+    int int_example = 1;
+    float float_example = 2.0;
+    double double_example = 2.0;
